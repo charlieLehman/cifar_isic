ac116bb98f2b74890da349d8e58fbbca9d3273e7
Charlie Lehman <charlie.k.lehman@gmail.com>
2016-10-12 20:34:24 -0400

    sensitivity and specificity test

--- cifar10_eval.py
+++ cifar10_eval.py
@@ -1,4 +1,4 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+ Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -120,11 +120,10 @@ def evaluate():
     # Get images and labels for CIFAR-10.
     eval_data = FLAGS.eval_data == 'test'
     images, labels = cifar10.inputs(eval_data=eval_data)
-    print(sess.run([labels]))
 
     # Build a Graph that computes the logits predictions from the
     # inference model.
-    logits = cifar10.inference(images)
+    logits = tf.nn.softmax(cifar10.inference(images))
 
     # Calculate predictions.
     top_k_op = tf.nn.in_top_k(logits, labels, 1)
